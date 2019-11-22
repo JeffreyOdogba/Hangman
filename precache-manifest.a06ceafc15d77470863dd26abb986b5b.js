ea47795b810070f68436977fb4d7ba9da7eec863
Jeffrey Odogba <odogbajeffrey@gmail.com>
2019-11-21 21:04:51 -0500

    Updates

---
+++ precache-manifest.a06ceafc15d77470863dd26abb986b5b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fa3fd08035fb723fb6555232743325",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "015517c46512dcca9339",
+    "url": "/Hangman/static/css/main.0de29797.chunk.css"
+  },
+  {
+    "revision": "701fc5313a0b948a5271",
+    "url": "/Hangman/static/js/2.050e6558.chunk.js"
+  },
+  {
+    "revision": "015517c46512dcca9339",
+    "url": "/Hangman/static/js/main.3d160a2d.chunk.js"
+  },
+  {
+    "revision": "48e35c28e5ea22d1c6d3",
+    "url": "/Hangman/static/js/runtime-main.3ae5c34d.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/Hangman/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/Hangman/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/Hangman/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/Hangman/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/Hangman/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/Hangman/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/Hangman/static/media/6.c80394c2.jpg"
+  }
+]);
